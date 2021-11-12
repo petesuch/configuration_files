e76327e8c245775e3f4688eb8956c018bf7d8b6f
Peter Suchsland <visiondoctor2020@gmail.com>
2021-11-12 18:34:08 +0700

    edits

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # configuration_files_all_programs
- A place where I can go to remember how to configure a program without re-inventing the wheel.
+ A place to go to remember how to configure a program without re-inventing the wheel.
+
--- regexforfilenames.py
+++ regexforfilenames.py
@@ -2,7 +2,7 @@ import re
 
 regex = r".PDF"
 
-test_str = ("2SA1943.pdf\n")
+test_str = ("2SA 1943.pdf\n")
 
 subst = ".pdf"
 
@@ -14,12 +14,12 @@ if result:
 
 regex = r"[\ \-\_\(\)]"
 
-test_str = ("2SA1943.pdf\n")
+test_str = ("2 SA 1943.pdf\n")
 
 subst = "-"
 
 result = re.sub(regex, subst, test_str, 0, re.MULTILINE)
 
 if result:
-    print (result)
+    print(result)
 
