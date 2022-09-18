4762a1fdad0db04ea162b7503f35c1d97c466391
Peter Suchsland <visiondoctor2020@gmail.com>
2022-09-18 17:54:27 +0700

    Update push_backup.sh

--- push_backup.sh
+++ push_backup.sh
@@ -1,3 +1,7 @@
+#  This script assumes you have two windows systems with cygwin running rsync on both,
+#  and want to sync two folders.  Not sure I'm ever going to use it again... though it 
+#  may prove to be instructional.
+
 if [ -e "//A15/Users/Peter/Pythonprojects" ] ; then
     /usr/bin/mount -f -o dos,noacl "//A15/Users/Peter/Pythonprojects" /mnt/Pythonprojects;
     sleep 5;
